15b81ae5d4094e50e764396c45658d0d98d49700
changseverus <61175460+changseverus@users.noreply.github.com>
2020-03-23 11:57:13 +0800

    Add files via upload

---
+++ R logistic regression.R
@@ -0,0 +1,23 @@
+library(stats)
+
+# load train and test set data
+dat<-read.table("train data.csv",header=TRUE,sep=",")
+test<-read.table("test data.csv",header=TRUE,sep=",")
+
+# set categorical variable
+dat$a<-factor(dat$a)
+dat$b<-factor(dat$b)
+dat$c<-factor(dat$c)
+dat$d<-factor(dat$d)
+
+# plot histogram to see the distribution of applicants' income, job type, credit card type
+hist(dat$g)
+hist(dat$i)
+hist(dat$t)
+
+# fit logistic regression model to train data
+model<-glm(formula=o~.,data=dat,family=binomial(link="logit"))
+summary(model)
+
+# predict result with test data
+x<-predict.glm(model,type="response",newdata=test)
